bb17d32aca74badeea43d63a05ee65e184e9529f
BMZ <blademasterzhang@126.com>
2017-11-14 18:22:57 +0800

    add mongoose

--- Imooc/app.js
+++ Imooc/app.js
@@ -1,39 +1,146 @@
 var express = require('express');
+var path = require('path')
+var mongoose = require('mongoose')
+var _=require('underscore')
+var Movie = require('./models/movie')
 var port = process.env.PORT||3000;
-
 var app = express();
-app.set('views','./views');
+
+mongoose.connect('mongodb://localhost/imooc')
+
+app.set('views','./views/pages');
 app.set('view engine','jade');
+//app.use(express.bodyParser())
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public')));
+app.locals.moment = require('moment')
 app.listen(port);
 
 console.log('imooc started on port '+port);
 
 app.get('/',function(req,res){
-	res.render('index',{
-		title:'imooc 首页'
+	Movie.fetch(function(err,movies){
+		if(err){
+			console.log(err)
+		}
+		res.render('index',{
+			title:'imooc 首页',
+			movies:movies
+		})
 	})
 })
 
 app.get('/movie/:id',function(req,res){
-	res.render('detail',{
-		title:'imooc 详情页'
+	var id = req.params.id
+	Movie.findById(id,function(err,movie){
+		res.render('detail',{
+			title:'imooc '+movie.title,
+			movie:movie
+		})
 	})
 })
 
 app.get('/admin/movie',function(req,res){
 	res.render('admin',{
-		title:'imooc 后台录入页'
+		title:'imooc 后台录入页',
+		movie:{
+			doctor:'',
+			country:'',
+			title:'',
+			year:'',
+			poster:'',
+			language:'',
+			flash:'',
+			summary:''
+		}
 	})
 })
 
+app.get('/admin/update/:id',function(req,res){
+	var id = req.params.id
+	if(id){
+		Movie.findById(id,function(err,movie){
+			res.render('admin',{
+				title:'imooc 后台更新页',
+				movie:movie
+			})
+		})
+	}
+})
+
+app.post('/admin/movie/new',function(res,req){
+	var id = req.body.movie._id
+	var movieObj = req.body.movie;
+	var _movie
+	if(id!=='undefined'){
+		Movie.findById(id,function(err,movie){
+			if(err){
+				console.log(err)
+			}
+			_movie = _.extend(movie,movieObj)
+			_movie.save(function(err,movie){
+				if(err){
+					console.log(err)
+				}
+				res.redirect('/movie/'+movie._id)
+			})
+		})
+	}else{
+		_movie = new Movie({
+			doctor:movieObj.doctor,
+			title:movieObj.title,
+			country:movieObj.country,
+			language:movieObj.language,
+			year:movieObj.year,
+			poster:movieObj.poster,
+			summary:movieObj.summary,
+			flash:movieObj.flash
+		})
+		_movie.save(function(err,movie){
+			if(err){
+				console.log(err)
+			}
+			res.redirect('/movie/'+movie._id)
+		})
+	}
+})
+
 app.get('/admin/list',function(req,res){
-	res.render('list',{
-		title:'imooc 后台列表页'
+	Movie.fetch(function(err,movies){
+		if(err){
+			console.log(err)
+		}
+		res.render('list',{
+			title:'imooc 后台列表页',
+			movies:movies
+		})
 	})
 })
 
 app.get('/api/admin/list',function(req,res){
 	res.json({
-		title:'imooc 后台列表页'
+		title:'imooc 后台列表页',
+		movies:[{
+			_id:1,
+			doctor:'何塞.帕迪利亚',
+			country:'美国',
+			title:'机械战警',
+			year:2014,
+			poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
+			language:'英语',
+			flash:'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
+			summary:'《机械战警》是由何塞·帕迪里亚执导，乔尔·金纳曼、塞缪尔·杰克逊、加里·奥德曼等主演的一部科幻电影，改编自1987年保罗·范霍文执导的同名电影。影片于2014年2月12日在美国上映，2014年2月28日在中国大陆上映。影片的故事背景与原版基本相同，故事设定在2028年的底特律，男主角亚历克斯·墨菲是一名正直的警察，被坏人安装在车上的炸弹炸成重伤，为了救他，OmniCorp公司将他改造成了生化机器人“机器战警”，代表着美国司法的未来。'
+		},{
+			_id:2,
+			doctor:'何塞.帕迪利亚',
+			country:'美国',
+			title:'机械战警',
+			year:2014,
+			poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
+			language:'英语',
+			flash:'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
+			summary:'《机械战警》是由何塞·帕迪里亚执导，乔尔·金纳曼、塞缪尔·杰克逊、加里·奥德曼等主演的一部科幻电影，改编自1987年保罗·范霍文执导的同名电影。影片于2014年2月12日在美国上映，2014年2月28日在中国大陆上映。影片的故事背景与原版基本相同，故事设定在2028年的底特律，男主角亚历克斯·墨菲是一名正直的警察，被坏人安装在车上的炸弹炸成重伤，为了救他，OmniCorp公司将他改造成了生化机器人“机器战警”，代表着美国司法的未来。'
+		}]
 	})
 })
